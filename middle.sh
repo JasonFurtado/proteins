b15b8cfc8de8e5e703baab99c0642d1695264e92
Jason Furtado <jfurtado@ou.edu>
2022-06-13 16:06:46 +0000

    Added documentation

--- middle.sh
+++ middle.sh
@@ -1,3 +1,7 @@
 # Script to print out the middle of a file
 
+# usage: bash middle.sh [filename] [number of lines for head] [number of lines for tail]
+# example:
+#	bash middle.sh octane.pdb 15 5
+
 head -n "$2" "$1" | tail -n "$3"
